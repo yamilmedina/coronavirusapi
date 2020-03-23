eb972756246a5bc331e017c12b7535c7b8e269d2
yamilmedina <yamilmedina@users.noreply.github.com>
2020-03-22 21:03:22 -0300

    adds correct jvm target for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ apply {
 
 group = "dev.yamil"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
