d21291dd77c5b8a96675f02245a52ad8daf69959
Yamil Medina <yamilmedina@users.noreply.github.com>
2021-05-24 11:14:32 -0400

    chore(release): test actions

---
+++ run.js
@@ -0,0 +1,15 @@
+const child = require("child_process");
+const fs = require("fs");
+
+const ENCODING_FORMAT = "utf-8"
+const latestTag = child.execSync('git describe --tags $(git rev-list --tags --max-count=1)').toString(ENCODING_FORMAT).replace(/.$/s, '')
+
+start = function () {
+    let newVersion = fs.readFileSync("chlog.md", ENCODING_FORMAT).split("=")[1]
+    console.log("Latest Tag: " + latestTag)
+    console.log("newVersion Tag: " + newVersion)    
+
+    fs.writeFileSync("chlog-" + newVersion + ".md", `${new Date()}`);
+};
+
+start()
